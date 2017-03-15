d5550062a8ecb4a254e80dbe7716e2778ed01cb6
corinne krych <corinnekrych@gmail.com>
2017-03-15 12:01:12 +0100

    add CocoaPods, node version pre-requisite in README

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 "use strict";
 
 module.exports = {
-  host: '',
-  username: '',
-  password: '',
-  prefix: 'app-art-',
+  host: 'https://testing.zeta.feedhenry.com',
+  username: 'testing-admin@example.com',
+  password: 'Password1',
+  prefix: 'app-art-jhellar-',
   retries: 3,
-  environment: '',
+  environment: 'qed1-mbaas1-test',
   iosPlatform: 'iOS Simulator,name=iPhone 7',
   iosUsername: '',
   iosPushBundleId: '',
@@ -14,10 +14,10 @@ module.exports = {
   iosPushTeam: '',
   iosPushAppIDName: '',
   iosPushDevelopment: true,
-  samlHost: '',
-  samlUsername:'',
-  samlPassword: '',
-  samlEntryPoint: '',
-  samlAuthContext: '',
-  samlCert: ''
+  samlHost: 'https://osm1-master1.feedhenry.net:8443/',
+  samlUsername:'admin',
+  samlPassword: 'Purple password1!',
+  samlEntryPoint: 'http://saml.osm1.feedhenry.net/simplesaml/saml2/idp/SSOService.php',
+  samlAuthContext: 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
+  samlCert: 'MIIDoTCCAomgAwIBAgIJALbevuF5olPDMA0GCSqGSIb3DQEBCwUAMGcxCzAJBgNVBAYTAkFVMRMwEQYDVQQIDApTb21lLVN0YXRlMSEwHwYDVQQKDBhJbnRlcm5ldCBXaWRnaXRzIFB0eSBMdGQxIDAeBgNVBAMMF3NhbWwub3NtMS5mZWVkaGVucnkubmV0MB4XDTE3MDMxMDE0MTM1MFoXDTI3MDMxMDE0MTM1MFowZzELMAkGA1UEBhMCQVUxEzARBgNVBAgMClNvbWUtU3RhdGUxITAfBgNVBAoMGEludGVybmV0IFdpZGdpdHMgUHR5IEx0ZDEgMB4GA1UEAwwXc2FtbC5vc20xLmZlZWRoZW5yeS5uZXQwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDIPc2ms3xbrJu7ZrKNi1M+kZw9jnkmC28FxrNteZmbAfjjd/uRp1fxJSMM9d28gu1IfQLKh/6XLvz1DkjFKphQYeT/269Zfzkj70ua3ft+r4a3Ce2ZznmDIaduAaaaTVTThJ2AqBJOOUJOXCe1sEl3f+L/0jCpXD3EK9yCzIsoB53mvqB7dd1xMcSDim5L2OxQ9C3jWZ9WdI/6DbUdUGaIzRnVlFyGZnDh1XEqXz971vgfFzhgumTDLTgvr1GxXli6tjbjwxJkmkooeSLkRi6KO25gzXQD1XbpBcvSO8805k8v0duAEpiCbyBIYPTeEIJxg8CUwaLCwi2sXIN2DdAfAgMBAAGjUDBOMB0GA1UdDgQWBBQLyoR/KBUJDx0MdG44qPK0lA+VOjAfBgNVHSMEGDAWgBQLyoR/KBUJDx0MdG44qPK0lA+VOjAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQDHz/i+5JiKZ3jQY/4sbgdYDKT0WWZzJkczBKj35Ma64sTOkXzSs7N6LNWfWYh5YRccve4il2obhtY/kbav4wAmzOiP+CmGzT2EF9x39UHuq30teifMecquVqYdcMRAB1gr/vnU9rkdxfyAcp8N82c5pIaDAXj5EqJ4Po7SkrRoZ32qGFD6gh8P//iluwflEdlmP3mfGicgaMWE5FWf+8EW7Umnp3X2B3fnLwKTt/cc++TsyoaxwOI8KIiViOBX5qtEzPJQiMqBtx6w4LHW81lrV0X4C6XaYQZiuj9HzPdOQBb2+Ee5s7TOslxUXg2Jer4pD4tD/KdXd9n713LtTWlN'
 };
